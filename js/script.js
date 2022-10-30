0377b6164bf17a04cd8e084ee824e467396510bc
Dimitri Freitas <freitasdimitri7@gmail.com>
2022-10-30 17:55:46 -0300

    Banner added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
-const menuBtn = document.querySelector('.menuMobile');
-const nav = document.querySelector('.navegacao');
-
-menuBtn.addEventListener('click', handleClick);
-menuBtn.addEventListener('touchstart', handleClick);
-
-function handleClick(e) {
-  e.preventDefault();
-  nav.classList.toggle('ativo');
-  menuBtn.classList.toggle('ativo');
+function menuMobile() {
+  const menuBtn = document.querySelector('.menuMobile');
+  const nav = document.querySelector('.navegacao');
+  
+  menuBtn.addEventListener('click', handleClick);
+  menuBtn.addEventListener('touchstart', handleClick);
+  
+  function handleClick(e) {
+    e.preventDefault();
+    nav.classList.toggle('ativo');
+    menuBtn.classList.toggle('ativo');
+  }
 }
+menuMobile();
+
+const banner = document.querySelector('.banner');
+const imgs = banner.querySelectorAll('img');
+const imgList = Array.from(imgs);
+
+const srcList = imgList.map(e => {
+  return e.getAttribute('src');
+})
+
+// console.log(srcList.length)
+// console.log(srcList);
