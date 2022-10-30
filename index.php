e068a831799c62e3d7af9c03333f8f4ae3a9d15c
Dimitri Freitas <freitasdimitri7@gmail.com>
2022-10-29 23:20:36 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <title>Olá, mundo!</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <main class="container">
+    <header class="header d-flex align-items-center justify-content-between border-bottom">
+      <img src="./img/assets/darkMode.svg" alt="">
+      <h1>Logo</h1>
+      <div class="d-flex redes">
+        <a href="https://github.com/Dimitri13Freitas/projetoIntegrador2"><img src="./img/social/github.svg" alt="Icon Logo Git-Hub"></a>
+        <a href="https://github.com/Dimitri13Freitas/projetoIntegrador2"><img src="./img/social/instagram.svg" alt="Icon Logo Git-Hub"></a>
+        <a href="https://github.com/Dimitri13Freitas/projetoIntegrador2"><img src="./img/social/twitter.svg" alt="Icon Logo Git-Hub"></a>
+      </div>
+    </header>
+    <nav class="navegacao">
+      <ul class="d-flex p-0 flex-row flex-wrap-reverse align-items-center justify-content-around">
+        <li class="p-2"><a class="p-2" href="">Filmes</a></li>
+        <li class="p-2"><a class="p-2" href="">Series</a></li>
+        <li class="p-2"><a class="p-2" href="">Livros</a></li>
+        <li class="p-2"><a class="p-2" href="">Animações</a></li>
+        <li class="p-2"><a class="p-2" href="">Contato</a></li>
+        <li class="p-2"><a class="p-2" href="">Normas de uso</a></li>
+        <li class="p-2"><a class="p-2" href="">Quem somos</a></li>
+      </ul>
+    </nav>
+  </main>
+
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+
+</html>
