b615ab5fec1250628b3e1a63d4f3eedbe0d3f5de
Dimitri Freitas <freitasdimitri7@gmail.com>
2022-11-10 12:48:01 -0300

    include com php e noticias adicionado

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Ficção cientifica</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/seila.css">
+</head>
+
+<body>
+  <main class='container main'>
+    <?php include 'nav.php';?>
+    <section class="bannerHome">
+      <h2 class="remove">.</h2>
+      <div class="banner d-flex flex-column justify-content-center">
+        <article>
+          <h2>Titulo Super Foda</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit consectetur, doloribus, accusantium tempore ducimus nam qui porro numquam animi, maxime.</p>
+        </article>
+      </div>
+    </section>
+    <div class="d-flex justify-content-xl-between justify-content-md-center justify-content-sm-center mt-4 flex-wrap noticiaContainer">
+      <section class="noticia rounded-3  border border-1 d-flex align-items-center">
+        <div class="noticiaSeila d-flex flex-column">
+          <small>Filmes</small>
+          <h2>Seila</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At nam molestias ipsa praesentium tenetur ea fugiat unde impedit, assumenda itaque deleniti quam facilis totam doloremque. Soluta quia omnis cupiditate minima.</p>
+          <a href="filmesPrincipais.html">Continuar lendo...</a>
+        </div>
+        <img src="img/banner/img2.jpg" alt="">
+      </section>
+      <section class="noticia rounded-3 border  border-1 d-flex align-items-center">
+        <div class="noticiaSeila d-flex flex-column">
+          <small>Filmes</small>
+          <h2>Seila</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At nam molestias ipsa praesentium tenetur ea fugiat unde impedit, assumenda itaque deleniti quam facilis totam doloremque. Soluta quia omnis cupiditate minima.</p>
+          <a href="filmesPrincipais.html">Continuar lendo...</a>
+        </div>
+        <img src="img/banner/img2.jpg" alt="">
+      </section>
+    </div>
+  </main>
+
+
+  <script src="js/script.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ nav.php
@@ -0,0 +1,46 @@
+<?php
+echo "<header class='header d-flex align-items-center justify-content-between border-bottom'>
+<div class='theme'>
+  <img src='./img/assets/notification.svg' alt=''>
+</div>
+<button class='menuMobile'>Menu<span class='hamburguer'></span></button>
+<h1>Logo</h1>
+<div class='d-flex redes'>
+  <a href='https://github.com/Dimitri13Freitas/projetoIntegrador2'><img src='./img/social/github.svg' alt='Icon Logo Git-Hub'></a>
+  <a href='https://github.com/Dimitri13Freitas/projetoIntegrador2'><img src='./img/social/instagram.svg' alt='Icon Logo Instagram'></a>
+  <a href='https://github.com/Dimitri13Freitas/projetoIntegrador2'><img src='./img/social/twitter.svg' alt='Icon Logo Twitter'></a>
+</div>
+</header>
+<nav class='navegacao'>
+<ul class='d-flex p-0 flex-row align-items-center justify-content-around'>
+  <li class='p-2'>Filmes
+    <ul>
+      <li><a href='filmesCosplay.html'>Cosplays</a></li>
+      <li><a href='filmesPrincipais.html'>Principais Obras</a></li>
+      <li><a href='filmesDestaques.html'>Destaques</a></li>
+      <li><a href='filmesLançamentos.html'>Lançamentos</a></li>
+    </ul>
+  </li>
+  <li class='p-2'><a class='p-2' href=''>Series</a></li>
+  <li class='p-2'>Livros
+    <ul>
+      <li><a href='livrosPrincipais.html'>Principais Autores</a></li>
+      <li><a href='livrosFavoritos.html'>Favoritos da Crítica</a></li>
+      <li><a href='livrosClassicos.html'>Clássicos</a></li>
+      <li><a href='livrosDestaques.html'>Destaques</a></li>
+    </ul>
+  </li>
+  <li class='p-2'>Animações
+    <ul>
+      <li><a href='animacoesDestaques.html'>Destaques</a></li>
+      <li><a href='animacoesClassicos.html'>Clássicos</a></li>
+      <li><a href='animacoesLançamentos.html'>Lançamentos</a></li>
+      <li><a href='animacoesPremiados.html'>Premiados</a></li>
+    </ul>
+  </li>
+  <li class='p-2'><a class='p-2' href='contato.html'>Contato</a></li>
+  <li class='p-2'><a class='p-2' href='normas.html'>Normas de uso</a></li>
+  <li class='p-2'><a class='p-2' href='quemSomos.html'>Quem somos</a></li>
+</ul>
+</nav>";
+?>
