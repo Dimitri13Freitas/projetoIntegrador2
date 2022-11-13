56e78c42086cff3141aef4fe29bc50fd05f9ffcb
Dimitri Freitas <freitasdimitri7@gmail.com>
2022-11-12 21:14:31 -0300

    include no footer

---
+++ footer.php
@@ -0,0 +1,39 @@
+<?php
+echo "<div class='bg-secondary pt-3 mb-0 footer'>
+<footer>
+  <h2 class='h2 p-5 pt-4 pb-3'>Mapa do site</h2>
+  <nav class='navFooter container py-0'>
+    <ul class='listaPrimaria d-flex pb-4 justify-content-between flex-wrap'>
+      <li class='p-2'>Filmes
+        <ul class='listaSecundaria p-0 m-0'>
+          <li><a href='filmesCosplay.html'>Cosplays</a></li>
+          <li><a href='filmesPrincipais.html'>Principais Obras</a></li>
+          <li><a href='filmesDestaques.html'>Destaques</a></li>
+          <li><a href='filmesLancamentos.html'>Lançamentos</a></li>
+        </ul>
+      </li>
+      <li class='p-2 '>Livros
+        <ul class='p-0 m-0 listaSecundaria'>
+          <li><a href='livrosPrincipais.html'>Principais Autores</a></li>
+          <li><a href='livrosFavoritos.html'>Favoritos da Crítica</a></li>
+          <li><a href='livrosClassicos.html'>Clássicos</a></li>
+          <li><a href='livrosDestaques.html'>Destaques</a></li>
+        </ul>
+      </li>
+      <li class='p-2'>Animações
+        <ul class='p-0 m-0 listaSecundaria'>
+          <li><a href='animacoesDestaques.html'>Destaques</a></li>
+          <li><a href='animacoesClassicos.html'>Clássicos</a></li>
+          <li><a href='animacoesLançamentos.html'>Lançamentos</a></li>
+          <li><a href='animacoesPremiados.html'>Premiados</a></li>
+        </ul>
+      </li>
+      <li class='p-2'><a class='p-2' href='contato.html'>Contato</a></li>
+      <li class='p-2'><a class='p-2' href='normas.html'>Normas de uso</a></li>
+      <li class='p-2'><a class='p-2' href='quemSomos.html'>Quem somos</a></li>
+    </ul>
+  </nav>
+</footer>
+<p class='py-4 text-center m-0'>Nome do Site &copy; Alguns direitos reservados</p>
+</div>";
+?>
