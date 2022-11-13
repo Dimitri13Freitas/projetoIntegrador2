a9a29925636b9444496548e5f174a258ecdb9dd9
Dimitri Freitas <freitasdimitri7@gmail.com>
2022-11-13 02:05:44 -0300

    Destaques de animação em andamento

---
+++ animacoesDestaques.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Ficção cientifica</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<!-- RETIRAR TAMANHO DAS IMAGENS NO CSS -->
+
+<body>
+  <div class='container main'>
+    <?php include 'nav.php';?>
+    <section class="bannerHome">
+      <h2 class="remove">.</h2>
+      <div class="banner d-flex flex-column justify-content-center rounded">
+        <article class='ms-sm-2 ms-md-4 ms-lg-6 ms-xl-6'>
+          <h2>Titulo Super Foda</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit consectetur, doloribus, accusantium tempore ducimus nam qui porro numquam animi, maxime.</p>
+        </article>
+      </div>
+    </section>
+    <div class='row'>
+      <div class='col-8'>
+        <article class='pt-3'>
+          <h2 class='h1 pt-5 m-0'>Titulo SuperFoda</h2>
+          <small>january 1, 2022 by Dimitri</small>
+          <p class='border-top border-2 pt-2 mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis doloribus maxime officia officiis eum laboriosam commodi sapiente dicta, quisquam a inventore quam autem.</p>
+        </article>
+      </div>
+    </div>
+    <div class="row destaques">
+      <section>
+        <img src="img/banner/img2.jpg" alt="Seila">
+      </section>
+
+    </div>    
+
+  </div>
+
+
+
+
+
+  <?php include 'footer.php';?>
+  
+
+  <script src="js/script.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
--- nav.php
+++ nav.php
@@ -32,7 +32,7 @@ echo "<header class='header d-flex align-items-center justify-content-between bo
   </li>
   <li class='p-2'>Animações
     <ul>
-      <li><a href='animacoesDestaques.html'>Destaques</a></li>
+      <li><a href='animacoesDestaques.php'>Destaques</a></li>
       <li><a href='animacoesClassicos.html'>Clássicos</a></li>
       <li><a href='animacoesLançamentos.html'>Lançamentos</a></li>
       <li><a href='animacoesPremiados.html'>Premiados</a></li>
